1739ca2c882b227697fbd8c1cb9e99f12d93a35a
vladchashush <vladchashush@gmail.com>
2024-06-20 20:24:42 +0400

    feat(core): Add header and main parts

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
 //Ther would be a scripts for index page
-let a = 12;
-let b = 22;
 
-function sum(first, second) {
-  return first + second;
-}
-
-console.log(sum(a, b));
-
-console.log("Hello world!!");
-
-sum(a, b);
+console.log("просто проверка работает ли загрузка");
